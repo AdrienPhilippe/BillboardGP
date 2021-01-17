bdb041d36d3e2a47d7e153ebe3315ece3885998e
GuerroisTheo <theo.guerrois@edu.esiee.fr>
2021-01-17 15:21:23 +0100

    mise en forme

--- 6Evaluation/Projet/billboard/transform_file.py
+++ 6Evaluation/Projet/billboard/transform_file.py
@@ -4,17 +4,22 @@ import re
 
 df_bilboard = pd.read_csv("test.csv", sep=",")
 
-df_b_cleared = df_bilboard.fillna("")
+for j in range(len(df_bilboard['title'])):
+    for i in df_bilboard['title']:
+        if i == df_bilboard['title'][j]:
+            pass
+
+# df_b_cleared = df_bilboard.fillna("")
 
 # print(df_b_cleared.head())
 
-for i in range(len(df_b_cleared['artist'])):
-    df_b_cleared['artist'][i] = re.sub(r"Featuring ", "",df_b_cleared['artist'][i])
-    df_b_cleared['artist'][i] = re.sub(r"(\w*[!@#$%^&*]+\w*)", "",df_b_cleared['artist'][i])
-    df_b_cleared['title'][i] = re.sub(r"(\w*[!@#$%^&*]+\w*)", "",df_b_cleared['title'][i])
+# for i in range(len(df_b_cleared['artist'])):
+#     df_b_cleared['artist'][i] = re.sub(r"Featuring ", "",df_b_cleared['artist'][i])
+#     df_b_cleared['artist'][i] = re.sub(r"(\w*[!@#$%^&*]+\w*)", "",df_b_cleared['artist'][i])
+#     df_b_cleared['title'][i] = re.sub(r"(\w*[!@#$%^&*]+\w*)", "",df_b_cleared['title'][i])
 
-print(df_b_cleared['title'][4857]) 
+# print(df_b_cleared['title'][4857]) 
 
 
 
-df_b_cleared.to_csv('out.csv',index=False)
+# df_b_cleared.to_csv('test.csv',index=False)
