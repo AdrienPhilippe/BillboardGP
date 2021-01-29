7e1a049fb51aa391bf9ca9d5d0552f8dffea75df
GuerroisTheo <theo.guerrois@edu.esiee.fr>
2021-01-29 15:21:19 +0100

    test

--- 6Evaluation/Projet/billboard/transform_file.py
+++ 6Evaluation/Projet/billboard/transform_file.py
@@ -4,10 +4,20 @@ import re
 
 df_bilboard = pd.read_csv("test.csv", sep=",")
 
-for j in range(len(df_bilboard['title'])):
-    for i in df_bilboard['title']:
-        if i == df_bilboard['title'][j]:
-            pass
+df = df_bilboard['title'].unique()
+liste = []
+dic_final = {}
+
+for i in range(len(df)):
+    for j in range(len(df_bilboard['title'])):
+        if df[i] == df_bilboard['title'][j]:
+            liste.append((df_bilboard['date'][j],df_bilboard['rank'][j]))
+    
+    print('nom de la musique {}, place dans la liste {} sur {}'.format(df[i], i, df.shape))
+    dic_final[str(df[i])] = liste
+    liste[:] = []
+
+print(dic_final)
 
 # df_b_cleared = df_bilboard.fillna("")
 
